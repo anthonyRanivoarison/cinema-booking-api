0df6f697786ef2c1153825f9ebb4738e40c06e7e
anthonyRanivoarison <antonitsiory@gmail.com>
2026-08-02 17:10:10 +0300

    refactor: put 204 status in reservation delete response

--- src/main/java/io/poja/cinebook/endpoint/rest/controller/reservation/ReservationController.java
+++ src/main/java/io/poja/cinebook/endpoint/rest/controller/reservation/ReservationController.java
@@ -5,6 +5,7 @@ import io.poja.cinebook.service.ReservationService;
 import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -29,6 +30,7 @@ public class ReservationController {
   }
 
   @DeleteMapping("/{id}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
   public void delete(@PathVariable UUID id) {
     service.delete(id);
   }

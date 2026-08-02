d15dac69140f8137112eed65b9ba61b8209779af
anthonyRanivoarison <antonitsiory@gmail.com>
2026-08-02 15:29:08 +0300

    refactor: inverse isPresent into isEmpty condition when finding reservation and refactor error message

--- src/main/java/io/poja/cinebook/service/ReservationService.java
+++ src/main/java/io/poja/cinebook/service/ReservationService.java
@@ -27,9 +27,9 @@ public class ReservationService {
   }
 
   public Reservation update(Reservation reservation, UUID id) {
-    if (repository.findById(id).isPresent()) {
+    if (repository.findById(id).isEmpty()) {
       throw new EntityNotFoundException(
-          String.format("Reservation to update with ID %s not found", reservation.id()));
+          String.format("Reservation to update with ID %s not found", id));
     }
     return mapper.toModel(repository.save(mapper.toEntity(reservation)));
   }
@@ -37,7 +37,7 @@ public class ReservationService {
   public void delete(UUID id) {
     if (!repository.existsById(id)) {
       throw new EntityNotFoundException(
-          String.format("Reservation to update with ID %s not found", id));
+          String.format("Reservation to delete with ID %s not found", id));
     }
     repository.deleteById(id);
   }
